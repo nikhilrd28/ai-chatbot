99decba0913957187c509fb39bce6d7b58ff45b4
nikhilrd28 <nikhilrd28@gmail.com>
2022-07-03 00:36:49 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# AI-chatbot
+Create an environment and activate it.
+Install all the required files i.e torch , Flask, nltk.
+Create a json file that holds the data need to be trained.
+train the json data and store it.
+run the file 
+To run the flask application on localhost type
+>flask run
+If running it on local server:
+>flask run --host=0.0.0.0
---
+++ app.py
@@ -0,0 +1,42 @@
+#import pyautogui
+from flask import Flask, render_template, request, jsonify
+import time
+import json
+import os
+
+import waitress
+from chat import get_response
+#import webbrowser,pyautogui
+from threading import Timer 
+
+app = Flask(__name__)
+
+
+@app.route("/",methods=["GET"])
+def index_get():
+    #pyautogui.hotkey('ctrl', 'w')
+    return render_template("base.html")
+
+@app.route("/predict",methods=["POST"])
+def predict():
+    text = request.get_json().get("message") 
+    #TODO: check if text is valid  
+    response = get_response(text)  
+    message = {"answer":response}
+    return jsonify(message)
+
+#def open_browser():
+    #print(request.remote_addr)
+    #webbrowser.open_new("http://127.0.0.1:5000/")
+
+if __name__ == "__main__":
+    #open_browser()
+    #app.run(debug=True)
+    #serve(app, host='0.0.0.0', port=5000)
+    app.debug = False
+    port = int(os.environ.get('PORT', 33507))
+    waitress.serve(app, port=port)
+    
+    
+
+
